460073b0824daf227e379bf347b298ecad0adb35
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-02-17 00:55:28 +0100

    started a new project

---
+++ note.md
@@ -0,0 +1,9 @@
+for syscall error  : 
+1 npm cache clean --force
+2 npm install
+3 cry and felt like a failure
+4 rename some shit in the node module : .acorn-dynamic-import-6VTlYv6U'
+
+
+# NOTES 
+Angular is an application-design framework and development platform for creating efficient and sophisticated single-page apps.
---
+++ src/app/user/footer/footer.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FooterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+function beforeEach(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+function expect(component: FooterComponent) {
+  throw new Error('Function not implemented.');
+}
+
