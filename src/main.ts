1baae97ee06f61e25c998690f5b9ea3e5178fb66
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-04-18 19:29:40 +0100

    little change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { KindeAngularModule } from "kinde-angular";
+//import { KindeAngularModule } from "kinde-angular";
 import { provideKinde } from "kinde-angular";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
