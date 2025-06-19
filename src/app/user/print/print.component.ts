aad07cdc06b145fd6f692ec1b9305ec53827b4f1
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2025-06-19 21:08:05 +0100

    added and fixed the print component

--- src/app/user/print/print.component.ts
+++ src/app/user/print/print.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { BookingService } from '../services/booking.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { BookingService } from "../services/booking.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-print',
-  templateUrl: './print.component.html',
-  styleUrls: ['./print.component.css']
+  selector: "app-print",
+  templateUrl: "./print.component.html",
+  styleUrls: ["./print.component.css"],
 })
 export class PrintComponent implements OnInit {
-createTicket;
-  constructor(
-    private BookingService:BookingService,
-    private route:Router
-  ) { }
+  createTicket;
+  constructor(private BookingService: BookingService, private route: Router) {}
 
   ngOnInit() {
-  
-    this.BookingService.cast.subscribe(
-      res=>this.createTicket=res,
-    )
-    if(!this.createTicket){
-      this.route.navigate([''])
+    this.BookingService.cast.subscribe((res) => (this.createTicket = res));
+    if (!this.createTicket) {
+      this.route.navigate([""]);
     }
   }
 
+  printTicket() {
+    const printContents = document.querySelector(".ticket").innerHTML;
+    const originalContents = document.body.innerHTML;
+
+    if (printContents) {
+      document.body.innerHTML = printContents;
+      window.print();
+      document.body.innerHTML = originalContents;
+      window.location.reload(); // to reload the original view
+    }
+  }
 }
